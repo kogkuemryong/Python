576a59bb65ba5578ac383107614bd3f2370c8f99
kogkuemryong <rmafud93@naver.com>
2020-09-05 23:42:03 +0900

    2020-09-05 commit

---
+++ practice/theater_module.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 일반 가격 \n",
+    "def price(people):\n",
+    "    print('{0}명 가격은 {1}원 입니다.'.format(people, people * 10000))\n",
+    "    \n",
+    "# 조조 할인 가격    \n",
+    "def price_morning(people):\n",
+    "    print('{0}명 조조 할인 가격은 {1}원 입니다.'.format(people, people * 6000))    \n",
+    "    \n",
+    "# 군인 할인 가격    \n",
+    "def price_soldier(people):\n",
+    "    print('{0}명 군인 할인 가격은 {1}원 입니다.'.format(people, people * 4000)) "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "theater_module"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
